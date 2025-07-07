7470f7807e68d6383f49c1a94f705078b702f951
maso0310 <balimon@hotmail.com>
2025-07-08 04:43:29 +0800

    更新Flex

--- app.py
+++ app.py
@@ -301,8 +301,10 @@ def handle_message(event):
                     }
                 ]
                 }
-            message = FlexSendMessage(alt_text='Flex Message', contents)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
+            message = FlexSendMessage(alt_text='Flex Message', contents=contents)
+            line_bot_api.reply_message(event.reply_token, message)
+        else:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
     except:
         print(traceback.format_exc())
         line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
